1267abc7e6a59151a78950897dde5eae341867eb
Mahabub Islam <marufhasan7702@gmail.com>
2026-04-17 02:30:53 +0600

    Complete Friend Details Part

--- src/Friends/Friends.jsx
+++ src/Friends/Friends.jsx
@@ -1,56 +1,69 @@
 
-const Friends = ({ Friend }) => {
+import { HashLoader } from "react-spinners";
+import UseFriend from "../Hooks/UseFriend";
+import { Link } from "react-router";
 
+const Friends = () => {
+    
+
+ const {friends,loading} =UseFriend()
 
     return (
+        loading ? (
+           <div className="flex justify-center ">
+             <HashLoader color="green"></HashLoader>
+           </div>
+        ) : (
+            <div  className='container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-9 mb-15'>
 
-        <div className='container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-9 mb-15'>
-
-            {
-                Friend.map(friend =>
-                    <div key={friend.id} className="card p-3 shadow-lg">
-
+                {friends.map(friend => (
+                    <Link to={`/friends/${friend.id}`}  key={friend.id} className="card p-3 shadow-lg">
 
                         <figure>
-                            <img className='w-[200px] h-[200px] rounded-full'
+                            <img
+                                className='w-[200px] h-[200px] rounded-full'
                                 src={friend.picture}
-                                alt={friend.name} />
+                                alt={friend.name}
+                            />
                         </figure>
+
                         <div className="card-body">
                             <div className="text-center space-y-2">
-                                <div className="font-bold text-2xl"> {friend.name} </div>
-                                <div><p>62d ago</p></div>
 
+                                <div className="font-bold text-2xl">
+                                    {friend.name}
+                                </div>
+
+                                <div>
+                                    <p>{friend.days_since_contact}d ago</p>
+                                </div>
 
                                 <div className="flex justify-center gap-2 font-bold">
-                                    {
-                                        friend.tags.map((tag, index) =>
-                                            <span key={index} className="badge badge-accent">{tag}</span>
-                                        )
-                                    }
-
+                                    {friend.tags.map((tag, index) => (
+                                        <span key={index} className="badge badge-accent">
+                                            {tag}
+                                        </span>
+                                    ))}
                                 </div>
-                                <div>
 
-                                    <span className={`badge ${friend.status === 'Overdue'
-                                            ? 'badge-error'
-                                            : friend.status === 'Almost due'
-                                                ? 'badge-warning'
-                                                : 'badge-success'
-                                        }`}>
-                                        {friend.status}
-                                    </span>
-                                </div>
+                                <span className={`badge ${friend.status === 'Overdue'
+                                        ? 'badge-error'
+                                        : friend.status === 'Almost due'
+                                            ? 'badge-warning'
+                                            : 'badge-success'
+                                    }`}>
+                                    {friend.status}
+                                </span>
+
                             </div>
-
-
-
                         </div>
-                    </div>
-                )
-            }
-        </div>
+
+                    </Link>
+                ))}
+
+            </div>
+        )
     );
-};
+}
 
 export default Friends;
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -9,7 +9,7 @@ const HomePage = () => {
     return (
         <div>
            <Banner Friend={Friend}></Banner>
-        <Friends Friend={Friend}></Friends>
+        <Friends></Friends>
  
            
         </div>
